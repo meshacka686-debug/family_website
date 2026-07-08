5e8c0841bef0899597547041289615ea7c1b4cdc
Meshack Ayuba <meshack@customer.lan>
2026-07-08 15:30:28 -0400

    Force SQLite database to fix deploy

--- family_website/settings.py
+++ family_website/settings.py
@@ -59,27 +59,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'family_website.wsgi.application'
 import os
 from pathlib import Path
-import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DATABASE_URL = os.environ.get("DATABASE_URL")
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
-if DATABASE_URL and DATABASE_URL.strip():
-    DATABASES = {
-        "default": dj_database_url.parse(
-            DATABASE_URL,
-            conn_max_age=600,
-            ssl_require=False
-        )
-    }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
 
 
 
