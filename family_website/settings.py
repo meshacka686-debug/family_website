b9ef6872f52ea16595ccf430d99a9b83a25785c4
Meshack Ayuba <meshack@customer.lan>
2026-07-08 15:12:53 -0400

    Fix DATABASE_URL empty case with SQLite fallback

--- family_website/settings.py
+++ family_website/settings.py
@@ -63,13 +63,25 @@ import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DATABASES = {
-    "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR}/db.sqlite3",  # fallback if DATABASE_URL is not set
-        conn_max_age=600,
-        ssl_require=False
-    )
-}
+# Database (SQLite fallback if DATABASE_URL is not set)
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=False
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+
 
 
 
