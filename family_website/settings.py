6b85759ad8a9aee0880df7b025ae354dbbcd0601
Meshack Ayuba <meshack@customer.lan>
2026-07-17 08:53:39 -0400

    fix template DIRS to include project-level templates

--- family_website/settings.py
+++ family_website/settings.py
@@ -43,21 +43,23 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'family_website.urls'
 
+
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "templates")],  # 👈 point to the top-level templates folder
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'family_website.wsgi.application'
 
