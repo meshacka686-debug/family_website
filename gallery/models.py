f714cf1dc7460b76ece33a6b050b7e679ac747cf
Meshack Ayuba <meshack@customer.lan>
2026-07-17 10:17:27 -0400

    add uploaded_at field to Photo model

--- gallery/models.py
+++ gallery/models.py
@@ -2,6 +2,20 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Photo(models.Model):
-    image = models.ImageField(upload_to="photos/")
+class Gallery(models.Model):
+    image = models.ImageField(upload_to="gallery/")
     caption = models.CharField(max_length=255)
+    uploaded_at = models.DateTimeField(auto_now_add=True)  # 👈 add this
+
+    def __str__(self):
+        return self.caption
+
+
+
+class Photo(models.Model):
+    image = models.ImageField(upload_to="gallery/")
+    caption = models.CharField(max_length=255)
+    uploaded_at = models.DateTimeField(auto_now_add=True)  # 👈 add here
+
+    def __str__(self):
+        return self.caption
